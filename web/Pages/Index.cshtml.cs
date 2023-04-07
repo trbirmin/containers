15d4bc49498adc221b8e6895fdc808380c867965
Trudi <innocent17@gmail.com>
2023-04-07 12:48:44 -0400

    starting dotnet app

---
+++ web/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly IConfiguration _config;
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(IConfiguration config, ILogger<IndexModel> logger)
+    {
+        _config = config;
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
